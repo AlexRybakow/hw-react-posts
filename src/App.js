972736afbf8228d67d78f9db2291818aeb2dc8e8
AlexRybakow <AlexRybakow@users.noreply.github.com>
2021-04-14 12:46:14 +0300

    add second post to feed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import Post from './components/post/index'
 
 const RossGeller = "https://i.pinimg.com/originals/20/13/a4/2013a4bece1fd4e40c261ea4a609c90a.png";
 
-const DoctorGeller = "https://img.buzzfeed.com/buzzfeed-static/static/2018-10/10/11/enhanced/buzzfeed-prod-web-05/enhanced-733-1539186461-15.png?downsize=900:*&output-format=auto&output-quality=auto";
+const DoctorGeller = "https://cdn3.whatculture.com/images/2016/01/e4PD8qSM.png"
 
+const Professor = "http://d1ydle56j7f53e.cloudfront.net/assets/news_images/The-Professor--1--111036.jpg";
+
+const LaCasaDePapel = "https://miro.medium.com/max/1120/0*XhURthSzEKmBSAMW.png";
 
 const App = () => {
   return (<>
-   <Post author={{
+  <div className='post-feed'>
+  <Post author={{
             name: "Ross Geller",
             photo: RossGeller,
             nickname: "@divorce-force"
          }}
          content="Why even Facebook don't believe that we were on a break??"
          image={DoctorGeller}
-         date={"12 апр."}
-         
-         
+         date={"12 апр."}       
 />
+<Post author={{
+            name: "Sergio Marquina",
+            photo: Professor,
+            nickname: "@professor"
+         }}
+         content="Hey,do you want interesting job?"
+         image={LaCasaDePapel}
+         date={"12 апр."}       
+/>
+  </div>
+   
     </>
     )
   }
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Post = ({ author, content, image, date, network }) => {
     return (
         <div className='container'>
-        <div className='user-avatar'><img src={author.photo}></img>       
+        <div className='user-avatar'><img src={author.photo} alt='avatar'></img>       
         </div>
         <div className='post-body'>
         <div className='user-info'>
@@ -14,7 +14,7 @@ const Post = ({ author, content, image, date, network }) => {
         </div>
         <div className='post-content'>
             <p className='post-text'>{content}</p>
-            <img className='post-image'src={image}></img>
+            <img className='post-image'src={image} alt='post-pic'></img>
         </div>
         </div>
         </div>
